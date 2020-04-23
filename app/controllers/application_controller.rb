add0190316908a99e8c118f673d15acc0a24a579
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-04-23 12:39:32 +0900

    修正V3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   private
-  っっw
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
