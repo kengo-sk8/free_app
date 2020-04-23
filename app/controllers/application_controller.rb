b4d849b146a7c1b68b918fd19c6a2bea6d4a711a
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-04-23 12:23:06 +0900

    besesick認証　v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+end
+
 end
