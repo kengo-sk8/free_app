4b03dbf223fed3602fadfb543b7d7029069636d6
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-04-23 12:32:02 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == '' && password == ''
     end
   end
 end
