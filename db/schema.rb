298ebdeb1ec3471f4db7f290e6678c3c5fd5fa6b
73katoshota <munsan446@gmail.com>
2020-04-24 11:16:58 +0900

    DBとアソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_074156) do
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "last_name_kana", null: false
-    t.string "email", null: false
-    t.string "phone_number"
-    t.string "password", null: false
-    t.integer "birthday_year_id", null: false
-    t.integer "birthday_moon_id", null: false
-    t.integer "birthday_day_id", null: false
-    t.text "self_introduce"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
