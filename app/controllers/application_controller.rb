c97efe35b43f00eac806f03d8834e8e4dc32f698
kengo922 <kengo.matsuno.922@gmail.com>
2020-04-25 11:09:08 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,27 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
 
-  private
+  before_action :basic_auth, if: :production?
+
+private
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == '' && password == ''
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+
+    def production?
+      Rails.env.production?
     end
   end
 end
 
+#   before_action :basic_auth
+
+#   private
+#   def basic_auth
+#     authenticate_or_request_with_http_basic do |username, password|
+#       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+#     end
+#   end
+# end
