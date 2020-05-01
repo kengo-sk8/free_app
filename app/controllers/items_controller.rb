f869cf5cf6abcea4e5b7cb3a062d8feb220096a5
kengo922 <kengo.matsuno.922@gmail.com>
2020-05-01 23:12:39 +0900

    v7

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+    # if @item.update(item_params)
+    #   redirect_to root_path
+    # else
+    #   render :edit
+    # end
   end
  
   private
--- app/views/items/new.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array! @delivery_methods do |delivery|
-  json.id   delivery.id
-  json.name delivery.name
-end
