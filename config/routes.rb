6b3b986e282277c991f0846cf6964d13938638e2
kengo922 <kengo.matsuno.922@gmail.com>
2020-05-02 22:24:06 +0900

    V9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :signups, only: [:index] do
     collection do
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
 
   root 'items#index'
   resources :items
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  collection do
+    get 'get_category_children', defaults: { format: 'json' }
+    get 'get_category_grandchildren', defaults: { format: 'json' }
+  end
 end
