20bc18690a59e3908eeff3883f7f771f9a0b7074
tadashi622 <62418207+tadashi622@users.noreply.github.com>
2020-05-03 12:59:13 +0900

    login　微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+  before_action :configre_permitted_parameters, if: :devise_controller?
 
   private
   def production?
@@ -11,7 +13,12 @@ class ApplicationController < ActionController::Base
       username == Rails.application.credentials[:basic_auth][:user] &&
       password == Rails.application.credentials[:basic_auth][:pass]
     end
+  end
+  
+  protected
 
+  def configre_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana,:birthday_year_id, :birthday_moon_id, :birthday_day_id])
   end
 end
 
